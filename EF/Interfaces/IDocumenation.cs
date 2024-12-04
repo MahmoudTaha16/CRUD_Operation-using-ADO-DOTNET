4add96631ad33aab5ca2c03efc68b695cd86da13
Mahmoud <MahmoudTaha1698@gmail.com>
2024-12-04 10:02:08 +0200

    Full CRUD operation And Their Documentation Using The Attribute

---
+++ EF/Interfaces/IDocumenation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace EF.Interfaces
+{
+    internal interface IDocumenation
+    {
+        public string Document();
+    }
+}
---
+++ EF/Interfaces/ITransaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Interfaces
+{
+    internal interface ITransaction
+    {
+        public bool TransactionBalance(int Id1, int Id2, decimal BalanceToTransferFrom1_2, ref string Message);
+    }
+}
